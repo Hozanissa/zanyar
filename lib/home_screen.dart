4458aba35bc86005b0bdf73643885f970cbdfd29
Hozanissa <hozanissa98@gmail.com>
2026-09-21 11:15:20 +0300

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,11 +10,92 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Color(0xFF1f4e4c),
         foregroundColor: Color(0xFFFFF8F0),
         toolbarHeight: 125,
-        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
 
         title: Text('Zanyar, your go-to travel guide!!'),
 
         //actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login Screen',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.teal,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Enter Your Username (Must be, letters, numbers, characters, etc..)",
+                  labelText: "Username",
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: TextField(
+                //controller: , //this needs to have a class. controller is used to accept data into the textfield.
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Enter Your Password",
+                  labelText: "Password",
+                  prefixIcon: Icon(Icons.key),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {}, child: Text('Login')),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Click here',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text('to forget your password'),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text('into your account'),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
