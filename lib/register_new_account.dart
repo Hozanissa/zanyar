a708bd25d2718b0c60aca9389e135e58ff8fc00f
Hozanissa <hozanissa98@gmail.com>
2026-09-22 11:07:59 +0300

    I have copied the appBar from login_screen and copied it into noth reset_password and register_new_account

--- lib/register_new_account.dart
+++ lib/register_new_account.dart
@@ -11,6 +11,18 @@ class RegisterNewAccount extends StatefulWidget {
 class _RegisterNewAccountState extends State<RegisterNewAccount> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1f4e4c),
+        foregroundColor: Color(0xFFFFF8F0),
+        toolbarHeight: 110,
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
+
+        title: Text('Zanyar, your go-to travel guide!!'),
+        centerTitle: true,
+
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+      ),
+    );
   }
 }
--- lib/reset_password.dart
+++ lib/reset_password.dart
@@ -12,6 +12,18 @@ class ResetPassword extends StatefulWidget {
 class _ResetPasswordState extends State<ResetPassword> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1f4e4c),
+        foregroundColor: Color(0xFFFFF8F0),
+        toolbarHeight: 110,
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
+
+        title: Text('Zanyar, your go-to travel guide!!'),
+        centerTitle: true,
+
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+      ),
+    );
   }
 }
