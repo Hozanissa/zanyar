0453f5529f28fc39ad0f8d5da0b3b75c6414dfc9
Hozanissa <hozanissa98@gmail.com>
2026-09-22 23:25:35 +0300

    Removed AppBar from Login_screen, register_new_account, and reset_password

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -12,17 +12,6 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1f4e4c),
-        foregroundColor: Color(0xFFFFF8F0),
-        toolbarHeight: 110,
-        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
-
-        title: Text('Zanyar, your go-to travel guide!!'),
-        centerTitle: true,
-
-        //actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
--- lib/register_new_account.dart
+++ lib/register_new_account.dart
@@ -11,20 +11,6 @@ class _RegisterNewAccountState extends State<RegisterNewAccount> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1f4e4c),
-        foregroundColor: Color(0xFFFFF8F0),
-        toolbarHeight: 110,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.home),
-        ),
-        title: Text('Zanyar, your go-to travel guide!!'),
-        centerTitle: true,
-        //actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
-      ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
