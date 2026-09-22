1fe242a807c02ae1b3710fe208fb92fec0e82b53
Hozanissa <hozanissa98@gmail.com>
2026-09-22 10:17:30 +0300

    only changed the classes' names and so on

--- lib/register_new_account.dart
+++ lib/register_new_account.dart
@@ -1,1 +1,16 @@
 //This class is to register a new account for the user.
+import 'package:flutter/material.dart';
+
+class RegisterNewAccount extends StatefulWidget {
+  const RegisterNewAccount({super.key});
+
+  @override
+  State<RegisterNewAccount> createState() => _RegisterNewAccountState();
+}
+
+class _RegisterNewAccountState extends State<RegisterNewAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/reset_password.dart
+++ lib/reset_password.dart
@@ -1,2 +1,17 @@
 //This class is created so that the user can
 //reset his/her password in case they forgot their password.
+import 'package:flutter/material.dart';
+
+class ResetPassword extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<ResetPassword> createState() => _ResetPasswordState();
+}
+
+class _ResetPasswordState extends State<ResetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
